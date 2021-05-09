0a79eded2082a3a59d1514b0264528b8b75b9c7e
SILGA Pinguedwindé fabrice <sliga.brice@gmail.com>
2021-05-09 19:21:00 +0200

    Processing form submission : created converter IngredientConverterByID for processing string to Ingredient instance

--- src/main/java/com/silga/dolocloud/controller/DesignDoloController.java
+++ src/main/java/com/silga/dolocloud/controller/DesignDoloController.java
@@ -5,10 +5,7 @@ import com.silga.dolocloud.model.Ingredient;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -34,7 +31,7 @@ public class DesignDoloController {
                 new Ingredient("RTPA", "Raan Tood Paale", Ingredient.Type.ALCOHOLIC),
                 new Ingredient("RTMA", "Raan Tood Maasga", Ingredient.Type.ALCOHOLIC),
                 new Ingredient("RAKO", "Raan Koom", Ingredient.Type.NON_ALCOHOLIC),
-                new Ingredient("RKMA", "Raan Koom Paale", Ingredient.Type.NON_ALCOHOLIC),
+                new Ingredient("RKPA", "Raan Koom Paale", Ingredient.Type.NON_ALCOHOLIC),
                 new Ingredient("RKMA", "Raan Koom Maasga", Ingredient.Type.NON_ALCOHOLIC)
         );
         Ingredient.Type[] types = Ingredient.Type.values();
@@ -49,6 +46,12 @@ public class DesignDoloController {
     public String showDesignDoloForm(Model model){
         model.addAttribute("dolo", new Dolo());
         return "design";
+    }
+
+    @PostMapping
+    public String processDolo(Dolo dolo){
+        log.info("Processing dolo: " + dolo);
+        return "redirect:/orders/current";
     }
 
     private Iterable<Ingredient> filterByType(List<Ingredient> ingredients, Ingredient.Type type){
