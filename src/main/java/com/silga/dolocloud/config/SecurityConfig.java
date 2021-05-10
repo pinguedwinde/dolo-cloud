4f10e1fbfc883b2118b7fff46be72c4a87455c32
SILGA Pinguedwindé fabrice <sliga.brice@gmail.com>
2021-05-11 00:37:28 +0200

    Securing requests : configurer bean SecurityFilterChain and customized login page

--- src/main/java/com/silga/dolocloud/config/SecurityConfig.java
+++ src/main/java/com/silga/dolocloud/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.silga.dolocloud.model.User;
 import com.silga.dolocloud.repository.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -11,6 +12,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -32,5 +34,19 @@ public class SecurityConfig {
                 return user;
             throw new UsernameNotFoundException("User '" + username + "' not found");
         };
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeRequests()
+                    .antMatchers("/design", "/orders").access("hasRole('USER')")
+                    .antMatchers("/", "/**").access("permitAll()")
+                .and()
+                    .formLogin()
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/design", true)
+                .and()
+                .build();
     }
 }
