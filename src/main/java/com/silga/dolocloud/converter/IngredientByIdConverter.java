e248a6d89bd4fdda3840cf9bf5f9243bc50e192b
SILGA Pinguedwindé fabrice <sliga.brice@gmail.com>
2021-05-12 13:43:01 +0200

    Activating profiles : used Spring profiles spring.actives.profile and @Profile, prod, dev...

--- src/main/java/com/silga/dolocloud/converter/IngredientByIdConverter.java
+++ src/main/java/com/silga/dolocloud/converter/IngredientByIdConverter.java
@@ -2,16 +2,15 @@ package com.silga.dolocloud.converter;
 
 import com.silga.dolocloud.model.Ingredient;
 import com.silga.dolocloud.repository.IngredientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
-
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 @Component
 public class IngredientByIdConverter implements Converter<String, Ingredient> {
     private final IngredientRepository ingredientRepository;
 
+    @Autowired
     public IngredientByIdConverter(IngredientRepository ingredientRepository) {
         this.ingredientRepository = ingredientRepository;
     }
--- src/main/java/com/silga/dolocloud/repository/IngredientRepository.java
+++ src/main/java/com/silga/dolocloud/repository/IngredientRepository.java
@@ -3,6 +3,7 @@ package com.silga.dolocloud.repository;
 import com.silga.dolocloud.model.Ingredient;
 import org.springframework.data.repository.CrudRepository;
 
+
 public interface IngredientRepository  extends CrudRepository<Ingredient, String> {
 
 }
