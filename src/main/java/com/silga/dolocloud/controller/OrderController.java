55848caa6502e8b72105f86b3c55d1cbc06f2dc8
SILGA Pinguedwindé fabrice <sliga.brice@gmail.com>
2021-05-12 09:53:33 +0200

    Creating your own configuration properties : Used @ConfigurationProperties and injected dolo.orders.pageSize by Configured the Spring Annotation Processor

--- src/main/java/com/silga/dolocloud/controller/OrderController.java
+++ src/main/java/com/silga/dolocloud/controller/OrderController.java
@@ -5,6 +5,9 @@ import com.silga.dolocloud.model.User;
 import com.silga.dolocloud.repository.OrderRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,16 +15,21 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 
 import javax.validation.Valid;
 
 @Slf4j
 @Controller
+@SessionAttributes("order")
 @RequestMapping("orders")
+@ConfigurationProperties(prefix = "dolo.orders")
 public class OrderController {
 
     private final OrderRepository orderRepository;
+    private int pageSize = 20;
+
     @Autowired
     public OrderController(OrderRepository orderRepository) {
         this.orderRepository = orderRepository;
@@ -31,6 +39,13 @@ public class OrderController {
     public String orderForm(Model model) {
         model.addAttribute("doloOrder", new DoloOrder());
         return "orderForm";
+    }
+
+    @GetMapping
+    public String ordersForUser( @AuthenticationPrincipal User user, Model model) {
+        Pageable pageable = PageRequest.of(0, pageSize);
+        model.addAttribute("orders", orderRepository.findByUserOrderByPlacedAtDesc(user,pageable));
+        return "orderList";
     }
 
     @PostMapping
--- src/main/java/com/silga/dolocloud/repository/OrderRepository.java
+++ src/main/java/com/silga/dolocloud/repository/OrderRepository.java
@@ -1,8 +1,13 @@
 package com.silga.dolocloud.repository;
 
 import com.silga.dolocloud.model.DoloOrder;
+import com.silga.dolocloud.model.User;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 public interface OrderRepository extends CrudRepository<DoloOrder, Long> {
 
+    List<DoloOrder> findByUserOrderByPlacedAtDesc(User user, Pageable pageable);
 }
